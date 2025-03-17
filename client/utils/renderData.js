22c2eb75ab3b95c1ca8b4e52e04dd9d5faf0dfcd
bogdan4iqk <samsonovbogdan22@gmail.com>
2025-03-17 23:03:08 +0300

    ready+

--- client/utils/renderData.js
+++ client/utils/renderData.js
@@ -6,11 +6,12 @@ export const renderAllUsers =  (users, renderTo) => {
 	let i = 0
 	users.forEach(user => {
 		i++
-		renderTo.insertAdjacentHTML(`beforeend`, `<li><div class="li${i}">${user.name}</div> <input type="text" class="input${i} input" placeholder="${user.name}"> <div class="edit${i} editBtn">V</div><div class="dlt${i} dltBtn" style="z-index: 100;">X</div></li>`)
+		renderTo.insertAdjacentHTML(`beforeend`, `<li class="list${i}"><div class="li${i}"><div class="user${i}">${user.name}</div></div> <input type="text" class="input${i} input" placeholder="${user.name}"> <div class="edit${i} editBtn">V</div><div class="dlt${i} dltBtn" style="z-index: 100;">X</div></li>`)
+		let i2 = i
 		document.querySelector(`.dlt${i}`).addEventListener('click', async () => {
 			await UserRequests.deleteUser(`${SERVER_URL}/api/deleteUser`, `${user._id}`)
+			document.querySelector(`.list${i2}`).remove()
 		})
-		let i2 = i
 		document.querySelector(`.li${i}`).addEventListener('click', () => {
 			
 			document.querySelector(`.input${i2}`).classList.add('zi')
@@ -20,6 +21,8 @@ export const renderAllUsers =  (users, renderTo) => {
 			let name = document.querySelector(`.input${i2}`).value
 				try {
 					await UserRequests.patchUser(`${SERVER_URL}/api/updateUser`, `${user._id}`, {name})
+					document.querySelector(`.user${i2}`).innerHTML = ''
+					document.querySelector(`.user${i2}`).innerHTML =`${name}`
 					document.querySelector(`.input${i2}`).classList.remove('zi')
 					document.querySelector(`.edit${i2}`).classList.remove('zi')
 					console.log(name)
